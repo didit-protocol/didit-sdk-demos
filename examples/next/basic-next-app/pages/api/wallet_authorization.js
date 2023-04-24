4fc660592e2a41dadec82de1e1e73d1d4b1ce962
Alberto <rosas.alberto.upc@gmail.com>
2023-04-24 12:45:06 +0800

    fix version didit

--- examples/next/basic-next-app/pages/api/wallet_authorization.js
+++ examples/next/basic-next-app/pages/api/wallet_authorization.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
-    const { scope, wallet_address, claims } = req.body;
+    const { wallet_address } = req.body;
+    const scope = process.env.SCOPE;
+    const claims = process.env.CLAIMS;
 
     const auth = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString("base64");
 
