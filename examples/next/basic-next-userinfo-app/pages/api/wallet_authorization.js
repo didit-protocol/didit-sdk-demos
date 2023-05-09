195f943b3c607494370e7a3d323896dd6317f132
Alberto <rosas.alberto.upc@gmail.com>
2023-05-09 12:03:32 +0800

    Removed application/json unnecesary

--- examples/next/basic-next-userinfo-app/pages/api/wallet_authorization.js
+++ examples/next/basic-next-userinfo-app/pages/api/wallet_authorization.js
@@ -6,11 +6,12 @@ const handler = async (req, res) => {
     const scope = process.env.SCOPE;
     const claims = process.env.CLAIMS;
 
-    const auth = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString("base64");
+    const auth = Buffer.from(
+      `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+    ).toString("base64");
 
     const headers = {
-      "Content-Type": "application/json",
-      "Authorization": `Basic ${auth}`,
+      Authorization: `Basic ${auth}`,
     };
 
     const data = {};
@@ -19,12 +20,18 @@ const handler = async (req, res) => {
     if (claims) data["claims"] = claims;
 
     try {
-        const wallet_authorization_response = await axios.post(process.env.API_URL + "wallet_authorization/", data, { headers });
-        res.status(wallet_authorization_response.status).json(wallet_authorization_response.data);
-      } catch (error) {
-        const errorData = error.response?.data || { message: error.message };
-        res.status(error.response?.status || 500).json(errorData);
-      }
+      const wallet_authorization_response = await axios.post(
+        process.env.API_URL + "wallet_authorization/",
+        data,
+        { headers }
+      );
+      res
+        .status(wallet_authorization_response.status)
+        .json(wallet_authorization_response.data);
+    } catch (error) {
+      const errorData = error.response?.data || { message: error.message };
+      res.status(error.response?.status || 500).json(errorData);
+    }
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
